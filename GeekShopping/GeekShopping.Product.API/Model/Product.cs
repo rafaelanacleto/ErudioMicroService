73f01d47543647d0b869a5356bfe349069c9fd61
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-09-21 20:42:02 -0300

    feat:(api) Criado entity Product Model

---
+++ GeekShopping/GeekShopping.Product.API/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Product.API.Model.Base;
+
+namespace GeekShopping.Product.API.Model
+{
+    public class Product : BaseEntity
+    {
+        public Product() { }
+
+        public Product(int id) { }
+
+
+    }
+}
